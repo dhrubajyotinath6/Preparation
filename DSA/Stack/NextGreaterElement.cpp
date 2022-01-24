c5d6be257fb180d2238e358e3cd949f6fd0d11f7
Dhruba <ndhrubajyoti195@gmail.com>
2022-01-24 23:24:24 +0530

    NextGreaterElement

---
+++ DSA/Stack/NextGreaterElement.cpp
@@ -0,0 +1,30 @@
+/*
+https://youtu.be/Du881K7Jtk8
+
+*/
+
+class Solution{
+
+    vector<int> nextGreaterElements(vector<int> &nums){
+
+        int n = nums.size();
+        vector<int> nge(n, -1);
+        stack<int> st;
+
+        for(int i = 2*n - 1; i >= 0; i--){
+
+            while(!st.empty() && st.top() <= nums[i%n]);{
+                st.pop();
+            }
+
+            if(i < n){
+                if(!st.empty()) nge[i] = st.top();
+            }
+            st.push(nums[i % n]);            
+        }
+        return nge;
+    }
+
+};
+
+
