a38d57a49e9891cca10790d35d38f7a07714ca99
Dhruba <ndhrubajyoti195@gmail.com>
2022-08-07 18:30:42 +0530

    Minimum Deletions in a String to make it a Palindrome

--- DSA/GROOKING DYNAMIC PROGRAMMING PATTERN/(d). PALINDROMIC SUBSEQUENCE/4.Minimum Deletions in a String to make it a Palindrome.cpp
+++ DSA/GROOKING DYNAMIC PROGRAMMING PATTERN/(d). PALINDROMIC SUBSEQUENCE/4.Minimum Deletions in a String to make it a Palindrome.cpp
@@ -37,3 +37,46 @@ So, our solution for a given string ‘st’ will be:
 
 
 */
+
+// bottom-up dynamic programming
+class MDSP
+{
+
+public:
+    int findMinimumDeletions(const string &st)
+    {
+        // subtracting the length of Longest Palindromic Subsequence from the length of
+        // the input string to get minimum number of deletions
+        return st.length() - findLPSLength(st);
+    }
+
+    int findLPSLength(const string &st)
+    {
+        // dp[i][j] stores the length of LPS from index 'i' to index 'j'
+        vector<vector<int>> dp(st.length(), vector<int>(st.length()));
+
+        // every sequence with one element is a palindrome of length 1
+        for (int i = 0; i < st.length(); i++)
+        {
+            dp[i][i] = 1;
+        }
+
+        for (int startIndex = st.length() - 1; startIndex >= 0; startIndex--)
+        {
+            for (int endIndex = startIndex + 1; endIndex < st.length(); endIndex++)
+            {
+                // case 1: elements at the beginning and the end are the same
+                if (st[startIndex] == st[endIndex])
+                {
+                    dp[startIndex][endIndex] = 2 + dp[startIndex + 1][endIndex - 1];
+                }
+                else
+                { // case 2: skip one element either from the beginning or the end
+                    dp[startIndex][endIndex] =
+                        max(dp[startIndex + 1][endIndex], dp[startIndex][endIndex - 1]);
+                }
+            }
+        }
+        return dp[0][st.length() - 1];
+    }
+};
