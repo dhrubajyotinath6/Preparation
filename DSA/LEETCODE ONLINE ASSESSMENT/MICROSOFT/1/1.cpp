568d0e72dc3aa9b2195d79a710ecccdb182663f2
Dhruba <ndhrubajyoti195@gmail.com>
2022-08-22 22:53:45 +0530

    LC REMOVE K DIGITS

--- DSA/LEETCODE ONLINE ASSESSMENT/MICROSOFT/1/1.cpp
+++ DSA/LEETCODE ONLINE ASSESSMENT/MICROSOFT/1/1.cpp
@@ -6,3 +6,85 @@
 2. REMOVE K DIGITS
 
 */
+
+/*
+https://leetcode.com/problems/remove-k-digits/solution/
+
+https://leetcode.com/problems/remove-k-digits/discuss/1779458/C%2B%2B-oror-Easy-To-Understand-oror-Stack-oror-Short-and-Simple
+
+https://leetcode.com/problems/remove-k-digits/discuss/88680/My-Easy-Understandable-C%2B%2B-Solution
+
+"10200"  -> 200
+K = 1,
+
+"10200" > 0
+K = 3
+
+"10"   -> 0
+1
+
+"10001"
+4
+
+1234567890 -> 0
+k = 9
+
+100200 -> 200
+K = 3
+
+100000 -> 0
+K=4, K = 1
+*/
+
+// REMOVE K DIGITS SOLUTION
+class Solution
+{
+public:
+    string removeKdigits(string num, int k)
+    {
+
+        // number of operation greater than length we return an empty string
+        if (num.length() <= k)
+            return "0";
+
+        // k is 0 , no need of removing /  preforming any operation
+        if (k == 0)
+            return num;
+
+        stack<char> st;
+
+        for (char n : num)
+        {
+
+            while (st.empty() == false && k > 0 && st.top() > n)
+            {
+                st.pop();
+                k--;
+            }
+
+            st.push(n);
+
+            // popping preceding zeroes
+            if (st.size() == 1 && n == '0')
+                st.pop();
+        }
+
+        for (int i = 0; i < k; i++)
+        {
+            if (st.empty() == false)
+                st.pop();
+        }
+
+        string ans = "";
+
+        while (st.size() > 0)
+        {
+            ans += st.top();
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans.empty() ? "0" : ans;
+    }
+};
