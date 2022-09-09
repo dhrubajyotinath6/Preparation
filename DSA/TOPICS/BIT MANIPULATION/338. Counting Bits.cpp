cb8e1b26d425735c5f17b39be769bc4f50cd56d0
Dhruba <ndhrubajyoti195@gmail.com>
2022-09-09 10:07:51 +0530

    lc 1996

--- DSA/TOPICS/BIT MANIPULATION/338. Counting Bits.cpp
+++ DSA/TOPICS/BIT MANIPULATION/338. Counting Bits.cpp
@@ -1,4 +1,6 @@
 /*
+https://leetcode.com/problems/counting-bits/
+
 Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n),
 ans[i] is the number of 1's in the binary representation of i.
 
@@ -25,3 +27,31 @@ Explanation:
 5 --> 101
 
 */
+
+class Solution {
+    
+    int noOf1s(int n){
+        
+        int count = 0;
+        
+        while(n > 0){
+            n = n&(n-1);
+            count++;
+        }
+        
+        return count;
+    }
+    
+public:
+    vector<int> countBits(int n) {
+        
+        vector<int> ans;
+        
+        for(int i = 0; i <= n; i++){
+            
+            ans.push_back(noOf1s(i));
+        }
+        
+        return ans;
+    }
+};
