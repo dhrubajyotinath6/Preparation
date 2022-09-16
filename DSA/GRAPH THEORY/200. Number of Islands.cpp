603c96e48c12c3dae6e751af46922cd3199eda45
Dhruba <ndhrubajyoti195@gmail.com>
2022-09-16 12:14:40 +0530

    lc 200

---
+++ DSA/GRAPH THEORY/200. Number of Islands.cpp
@@ -0,0 +1,60 @@
+//https://leetcode.com/problems/number-of-islands/
+
+----------------------------------------------
+Example 1:
+
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+Example 2:
+
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+-------------------------------------
+
+class Solution {
+    
+    void dfs(vector<vector<char>> &matrix, int i, int j){
+        
+        if(i < 0 || i >= matrix.size() || j < 0 || j >= matrix[0].size() || matrix[i][j] == '0') 
+            return;
+        
+        matrix[i][j] = '0';
+        
+        dfs(matrix, i, j + 1);
+        dfs(matrix, i + 1, j);
+        dfs(matrix, i , j - 1);
+        dfs(matrix, i - 1, j);
+        
+    }
+    
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        
+        int row = grid.size(), col = grid[0].size();
+        
+        int count = 0;
+        
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                
+                if(grid[i][j] == '1'){
+                    count++;
+                    
+                    dfs(grid,i,j);                    
+                }
+            }
+        }
+        
+        return count;
+    }
+};
