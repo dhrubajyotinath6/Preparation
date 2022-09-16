89ef6a475d1867a807e1afe69e14045d1d86275a
Dhruba <ndhrubajyoti195@gmail.com>
2022-09-16 08:22:40 +0530

    189. Rotate Array

---
+++ DSA/ALGO STUDY PLAN/TWO POINTERS/189. Rotate Array.cpp
@@ -0,0 +1,48 @@
+/*
+https://leetcode.com/problems/rotate-array/
+
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+*/
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+        if(nums.size() <= 1) return;
+        
+        k = k % nums.size();
+        
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
+
+/*
+Your input
+[-1]
+2
+
+Output
+[-1]
+
+Expected
+[-1]
+
+*/
