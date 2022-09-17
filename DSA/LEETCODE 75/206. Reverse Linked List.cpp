dcfa0169be4e57d04f090c59473d029c8594a352
Dhruba <ndhrubajyoti195@gmail.com>
2022-09-17 08:41:16 +0530

    lc 206

---
+++ DSA/LEETCODE 75/206. Reverse Linked List.cpp
@@ -0,0 +1,49 @@
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+
+=>
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+        if(head == NULL) return NULL;
+        
+        ListNode *prev = NULL, *next = NULL, *curr = head;
+        
+        while(curr != NULL){
+            next = curr->next;
+            curr->next = prev;
+            
+            prev = curr;
+            curr = next;
+        }
+        
+        return prev;
+    }
+};
