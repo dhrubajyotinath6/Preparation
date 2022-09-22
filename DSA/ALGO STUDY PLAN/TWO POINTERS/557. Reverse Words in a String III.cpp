0452c9ee0baa78279b6746098be23cd43238326a
Dhruba <ndhrubajyoti195@gmail.com>
2022-09-22 11:24:39 +0530

    lc 557

--- DSA/ALGO STUDY PLAN/TWO POINTERS/557. Reverse Words in a String III.cpp
+++ DSA/ALGO STUDY PLAN/TWO POINTERS/557. Reverse Words in a String III.cpp
@@ -13,3 +13,38 @@ Input: s = "God Ding"
 Output: "doG gniD"
 
 */
+
+class Solution {    
+    void rev(string &s, int i, int j){
+        
+        while(i <= j){
+            char tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
+            i++,j--;
+        }        
+    }
+public:
+    string reverseWords(string s) {
+        
+        int lastSpace = -1;        
+        
+        for(int i = 0; i <= s.size(); i++){
+ 
+            if(s[i] == ' ' ||  i == s.size() ){
+                
+                int startIndex = lastSpace+1;
+                int endIndex = i - 1;
+                
+                rev(s,startIndex,endIndex);
+                
+                lastSpace = i;
+                
+            }
+            
+           
+        }
+        
+        return s;
+    }
+};
