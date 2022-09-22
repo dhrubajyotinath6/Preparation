b17d5646fe47d08a4329f4ffdc6d5d61755df16f
Dhruba <ndhrubajyoti195@gmail.com>
2022-09-22 11:31:34 +0530

    lc 19

---
+++ DSA/ALGO STUDY PLAN/TWO POINTERS/19. Remove Nth Node From End of List.cpp
@@ -0,0 +1,58 @@
+/*
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+
+
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+
+
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+        ListNode* start = new ListNode();
+        start->next = head;
+        
+        ListNode *fast = start, *slow= start;
+        
+        for(int i = 1; i <= n; i++){
+            fast = fast->next;
+        }
+        
+        while(fast->next != NULL){
+            fast = fast->next;
+            slow = slow->next;
+        }
+        
+        slow->next = slow->next->next;
+        
+        return start->next;
+    }
+};
