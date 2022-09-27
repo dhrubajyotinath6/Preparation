c11cda729bb13608b287904c735230a9199badbf
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2022-09-27 09:15:15 +0530

    Create 148. Sort List.cpp

---
+++ DSA/LEETCODE TOP INTERVIEW QUESTIONS/148. Sort List.cpp
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/sort-list/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+/*
+https://leetcode.com/problems/sort-list/discuss/1795126/C%2B%2B-oror-Merge-Sort-oror-2-Pointer-oror-Easy-To-Understand
+
+https://leetcode.com/problems/sort-list/discuss/1795343/C%2B%2Bor-Detailed-Explanation-w-Comments-or-Easy-to-understand-code
+
+*/
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        
+    }
+};
