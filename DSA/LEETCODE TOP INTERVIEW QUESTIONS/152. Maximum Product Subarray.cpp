a48bcde7963a83e84af24c5bded90c11f4c362df
Dhruba <ndhrubajyoti195@gmail.com>
2022-10-01 12:05:08 +0530

    lc 152

--- DSA/LEETCODE TOP INTERVIEW QUESTIONS/152. Maximum Product Subarray.cpp
+++ DSA/LEETCODE TOP INTERVIEW QUESTIONS/152. Maximum Product Subarray.cpp
@@ -19,3 +19,37 @@ Example 2:
 Input: nums = [-2,0,-1]
 Output: 0
 Explanation: The result cannot be 2, because [-2,-1] is not a subarray
+
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        
+        if(nums.size() == 0) return 0;
+        
+        int max_so_far = nums[0];
+        int min_so_far = nums[0];
+        
+        int res = max_so_far;
+        
+        for(int i = 1; i < nums.size(); i++){
+            int curr = nums[i];
+            
+            int temp = max(curr, max(max_so_far*curr, min_so_far*curr));
+            
+            min_so_far = min(curr, min(max_so_far*curr, min_so_far*curr)); 
+            
+            max_so_far = temp;
+            
+            res = max(res, max_so_far);
+        }
+        
+        return res;
+    }
+};
+
+/*
+
+tc - O(N)
+sc - O(1)
+*/
