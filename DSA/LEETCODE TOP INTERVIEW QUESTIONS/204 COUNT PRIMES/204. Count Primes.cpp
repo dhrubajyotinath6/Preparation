990deb8b623a165912d05e063c0a1cb056f5c04a
Dhruba <ndhrubajyoti195@gmail.com>
2022-10-01 11:21:09 +0530

    lc 204

---
+++ DSA/LEETCODE TOP INTERVIEW QUESTIONS/204 COUNT PRIMES/204. Count Primes.cpp
@@ -0,0 +1,48 @@
+//https://leetcode.com/problems/count-primes/
+
+//https://leetcode.com/problems/count-primes/solution/
+
+Approach: Sieve of Eratosthenes
+
+//https://gist.github.com/SuryaPratapK/909585384607cb0502901e9e51f6e30e
+//https://www.youtube.com/watch?v=pKvGYOnO9Ao
+
+class Solution {
+    
+    void SieveOfEratosthenes(int n, int &count){ 
+        
+	// Create a boolean array "prime[0..n]" and initialize 
+	// all entries it as true. A value in prime[i] will 
+	// finally be false if i is Not a prime, else true. 
+	bool prime[n+1]; 
+	memset(prime, true, sizeof(prime)); 
+
+	for (int p=2; p*p<=n; p++) 
+	{ 
+		// If prime[p] is not changed, then it is a prime 
+		if (prime[p] == true) 
+		{ 
+			// Update all multiples of p greater than or 
+			// equal to the square of it 
+			// numbers which are multiple of p and are 
+			// less than p^2 are already been marked. 
+			for (int i=p*p; i<=n; i += p) 
+				prime[i] = false; 
+		} 
+	} 
+
+	//all prime numbers 
+	for (int p=2; p<n; p++) 
+	if (prime[p]) 
+		count++;
+} 
+
+public:
+    int countPrimes(int n) {
+        
+        int count = 0;
+        SieveOfEratosthenes(n, count);
+        
+        return count;
+    }
+};
--- DSA/LEETCODE TOP INTERVIEW QUESTIONS/204. Count Primes.cpp
+++
@@ -1,5 +0,0 @@
-//https://leetcode.com/problems/count-primes/
-
-//https://leetcode.com/problems/count-primes/solution/
-
-Approach: Sieve of Eratosthenes
