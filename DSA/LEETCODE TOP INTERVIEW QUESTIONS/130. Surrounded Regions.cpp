b6ac19d43a1a750ebbaf4ce2443b363f5b79225a
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2022-11-11 15:38:46 +0530

    Update 130. Surrounded Regions.cpp

--- DSA/LEETCODE TOP INTERVIEW QUESTIONS/130. Surrounded Regions.cpp
+++ DSA/LEETCODE TOP INTERVIEW QUESTIONS/130. Surrounded Regions.cpp
@@ -2,6 +2,10 @@
 
 //https://leetcode.com/problems/surrounded-regions/solution/  --VERY IMP
 
+Given an m x n matrix board containing 'X' and 'O', capture all regions that are 4-directionally surrounded by 'X'.
+
+A region is captured by flipping all 'O's into 'X's in that surrounded region.
+
 
 class Solution {
 public:
