009f22b37ad8b7a0b1049877c8debb43bccdbb81
Dhruba <ndhrubajyoti195@gmail.com>
2023-01-11 23:11:07 +0530

    generate all binary strings

---
+++ DSA/STRIVER/A-Z/RECURSION/SUBSEQUENCES/Generate all binary strings.cpp
@@ -0,0 +1,16 @@
+/*
+Given an integer N , generate all binary strings of size N which do not contain consecutive 1s.
+
+A binary string is that string which contains only 0 and 1.
+
+Example 1:
+
+Input:
+N = 3
+Output:
+000 , 001 , 010 , 100 , 101
+Explanation:
+None of the above strings contain consecutive 1s. "110" is not an answer 
+as it has '1's occuring consecutively.
+*/
+
