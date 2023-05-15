d6d920e81702f5efb5ddbe19c3f1c53e453c18a0
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-05-15 11:57:56 +0530

    Update 1021. Remove Outermost Parentheses.cpp
    optimized solution added

--- DSA/TOPICS/STACK && QUEUE/1021. Remove Outermost Parentheses.cpp
+++ DSA/TOPICS/STACK && QUEUE/1021. Remove Outermost Parentheses.cpp
@@ -86,3 +86,36 @@ Expected
 
 */
 
+OPTIMIZED SOLUTION ; TC -> O(N),  SC -> O(1)
+ 
+ class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        
+        int bal = 0;
+        string ans = "";
+        
+        for(auto& ch : s){
+            
+            if(ch == '('){
+                
+                if(bal != 0){
+                    ans += ch;
+                }
+                
+            }else{ // ch == ')'
+                
+                if(bal > 1){
+                    ans += ch;
+                }
+            }
+            
+            bal += (ch == '(') ? 1:-1;
+            
+            }
+        
+        
+        return ans;
+    }
+};
+
