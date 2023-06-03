9445e6385896848dd00c329e39104e8f5e51a977
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-06-04 03:09:17 +0530

    Update 1021. Remove Outermost Parentheses.cpp

--- DSA/TOPICS/STACK && QUEUE/1021. Remove Outermost Parentheses.cpp
+++ DSA/TOPICS/STACK && QUEUE/1021. Remove Outermost Parentheses.cpp
@@ -117,5 +117,23 @@ public:
         
         return ans;
     }
+  
+  
+  //https://leetcode.com/problems/remove-outermost-parentheses/discuss/270022/JavaC%2B%2BPython-Count-Opened-Parenthesis
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+       
+        int opened = 0;
+        string res;
+        
+        for(auto &c : s){
+            if(c == '(' && opened++ > 0) res += c;
+            if(c == ')' && --opened > 0) res += c;
+        }
+        
+        return res;
+    }
+};
 };
 
