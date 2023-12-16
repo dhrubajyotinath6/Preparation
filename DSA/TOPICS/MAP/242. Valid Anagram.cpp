426e3a16b83e6cff4dc3ba724e52e11785bc98d9
Dhruba <ndhrubajyoti195@gmail.com>
2023-12-16 10:30:22 +0530

    lc 242

---
+++ DSA/TOPICS/MAP/242. Valid Anagram.cpp
@@ -0,0 +1,43 @@
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        if(s.size() != t.size()) return false;
+        
+        vector<int> freq(26,0);
+        
+        for(int i = 0; i < s.size(); i++){
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
+        
+        
+        for(int i = 0; i < 26; i++){
+            if(freq[i]) return false;
+        }
+        
+        return true;
+    }
+};
